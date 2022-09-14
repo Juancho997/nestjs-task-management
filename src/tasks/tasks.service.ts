c32a7e72be9f20444587e02d59fe580851d02089
Juan Bonino <tomasboni9@gmail.com>
2022-09-14 11:11:24 -0300

    feature: update-tasks-status validators

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,5 +1,5 @@
 import { v4 as uuid } from 'uuid';
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTaskFilterDto } from './dto/get-tasks-filter.dto';
@@ -35,7 +35,13 @@ export class TasksService {
     };
 
     getTaskById(id: string): Task {
-        return this.tasks.find((task) => task.id === id);
+        const found = this.tasks.find((task) => task.id === id);
+
+        if (!found) {
+            throw new NotFoundException(`Task with ID : ${id} not found!`);
+        }
+
+        return found;
     };
 
     createTask(createTaskDto: CreateTaskDto): Task {
@@ -55,7 +61,8 @@ export class TasksService {
     };
 
     deleteTask(id: string) {
-        this.tasks = this.tasks.filter(task => task.id !== id)
+        const found = this.getTaskById(id);
+        this.tasks = this.tasks.filter(task => task.id !== found.id);
         console.log(`Task ID : ${id} deleted!`)
     };
 
