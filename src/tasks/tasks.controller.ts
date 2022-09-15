f1f8bdef5fa9caccf3c53c28e00112fa9bf6ee49
Juan Bonino <tomasboni9@gmail.com>
2022-09-15 16:40:25 -0300

    fix: minor syntax fix

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -3,7 +3,6 @@ import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTaskFilterDto } from './dto/get-tasks-filter.dto';
 import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
 import { Task } from './task.entity';
-import { TaskStatus } from './task-status.enum';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
