08934c881526733b69e99c1b0c9b24e3cc472e7b
Juan Bonino <tomasboni9@gmail.com>
2022-09-15 16:41:06 -0300

    feature: auth module, service, controller and User entity added

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,14 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+
+import { AuthService } from './auth.service';
+
+
+@Controller('auth')
+export class AuthController {
+    constructor(private authService: AuthService) { }
+
+    // @Get()
+
+
+
+}
